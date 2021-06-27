e396a62892ce83192f4e8561ca3bf727a6c4c2f5
Richard Fine <richardf@unity3d.com>
2021-06-26 21:04:10 -0400

    Convert from unittest to pytest

--- tests/fusion_test.py
+++ tests/fusion_test.py
@@ -1,17 +1,11 @@
-import unittest
 from calculator.PersonaFusionCalculator import PersonaFusionCalculator
 
-
-class FusionTests(unittest.TestCase):
-    calculator = PersonaFusionCalculator()
-
-    def test_simple_fuse_two_personas(self):
-        result = self.calculator.get_fusion_result(['Silky', 'Arsene'])
-        self.assertEqual(result, 'Succubus')
-
-        result = self.calculator.get_fusion_result(['Silky', 'Pixie'])
-        self.assertEqual(result, 'Clotho')
+calculator = PersonaFusionCalculator()
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_simple_fuse_two_personas():
+    result = calculator.get_fusion_result(['Silky', 'Arsene'])
+    assert result == 'Succubus'
+
+    result = calculator.get_fusion_result(['Silky', 'Pixie'])
+    assert result == 'Clotho'
